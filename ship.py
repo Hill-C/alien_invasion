73e56c0de6d573e0b9b105db6fe8617caf92662a
chen <TenetAcid@163.com>
2019-02-27 15:11:57 +0800

    finish define ship

--- ship.py
+++ ship.py
@@ -1,7 +1,8 @@
 import pygame
 class Ship:
-    def __init__(self,ai_settings, screen):
+    def __init__(self, ai_settings, screen):
         self.screen = screen
+        self.ai_settings = ai_settings
         self.image = pygame.image.load('images/ship1.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
@@ -11,11 +12,13 @@ class Ship:
 
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
     def update(self):
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
