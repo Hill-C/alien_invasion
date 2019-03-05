35ac4bee3c2b252266b73f21eae5e58b01c61a60
chen <TenetAcid@163.com>
2019-03-05 15:28:58 +0800

    hide play button

--- game_functions.py
+++ game_functions.py
@@ -56,13 +56,15 @@ def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
             break
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
         stats.reset_stats()
         stats.game_active = True
         aliens.empty()
         bullets.empty()
         create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
+
 
 
 def create_alien(ai_settings, screen, aliens, alien_number, row_number):
